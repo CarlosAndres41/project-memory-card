1c75c909bff192dc6cd6aed4a9fc868dcf87a5cf
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-26 00:09:57 -0500

    Add coolImages package

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import Header from './components/Header';
 import Card from './components/Card';
 
+const coolImages = require('cool-images');
+
 function App() {
+    const images = coolImages.many(100, 200, 8);
+
     return (
         <div className='App'>
             <Header />
