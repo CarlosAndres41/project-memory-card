cb9b3f19142d10878f6ef9e321dcb85c050f707c
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-26 01:25:02 -0500

    Add container by selecting .gameboard

--- memory-card-app/src/utils/shuffleDivs.js
+++ memory-card-app/src/utils/shuffleDivs.js
@@ -1,4 +1,6 @@
-function shuffleDivs(container) {
+function shuffleDivs() {
+    let container = document.querySelector('.gameboard');
+
     for (let i = container.children.length; i >= 0; i--) {
         container.appendChild(container.children[(Math.random() * i) | 0]);
     }
