377be3806060fcf99f3191b4ff652f69d4304415
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-25 22:02:56 -0500

    Create Scoreboard component

---
+++ memory-card-app/src/components/Scoreboard.js
@@ -0,0 +1,16 @@
+const Scoreboard = () => {
+    return (
+        <div className='scoreboard'>
+            <div className='level'>
+                <span>Level:</span>
+                <span>1</span>
+            </div>
+            <div className='score'>
+                <span>Score:</span>
+                <span>0</span>
+            </div>
+        </div>
+    );
+};
+
+export default Scoreboard;
