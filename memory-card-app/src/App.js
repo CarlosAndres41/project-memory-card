7137d209bc9d3e7848cf5dd97161ce73ade2936b
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-25 23:21:44 -0500

    Import and add Card to App

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
     return (
         <div className='App'>
             <Header />
+            <div className='gameboard'>
+                <Card />
+            </div>
         </div>
     );
 }
