f63a22b4d980abe2ac2e812667fb7c77f9097ee9
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-25 21:53:49 -0500

    Create Logo component

---
+++ memory-card-app/src/components/Logo.js
@@ -0,0 +1,13 @@
+const Logo = () => {
+    return (
+        <div className='name-logo'>
+            <h1>
+                <span>
+                    <img src='' alt='logo' />
+                </span>
+            </h1>
+        </div>
+    );
+};
+
+export default Logo;
