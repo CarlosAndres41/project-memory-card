727a82594b7c5766564491142843df8fa19e917e
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-26 01:25:45 -0500

    Add shuffleDivs module to each card

--- memory-card-app/src/utils/AddEvListeners.js
+++ memory-card-app/src/utils/AddEvListeners.js
@@ -1,12 +1,12 @@
+import shuffleDivs from './shuffleDivs';
+
 function addListeners() {
     window.addEventListener('DOMContentLoaded', () => {
         let cards = document.querySelectorAll('.card');
         cards = Array.from(cards);
 
         cards.forEach((card) => {
-            card.onclick = () => {
-                console.log('Card clicked');
-            };
+            card.onclick = shuffleDivs;
         });
     });
 }
