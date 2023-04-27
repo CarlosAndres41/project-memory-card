71c18810e8193cbdf15902eed04e94de779ea55c
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-27 01:20:37 -0500

    Add logic for losing the game when a card is clicked twice.

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -4,17 +4,21 @@ import './App.css';
 import Header from './components/Header';
 import Card from './components/Card';
 
-import addListeners from './utils/AddEvListeners';
-
 const coolImages = require('cool-images');
 const images = coolImages.many(200, 200, 8);
 
 function App() {
     const [score, setScore] = useState(0);
     const [level, setLevel] = useState(1);
+    const [clickedCards, setClickedCards] = useState([]);
 
-    function updateScore() {
-        setScore(score + 1);
+    function updateScore(e) {
+        if (!clickedCards.includes(e.target.attributes.index.value)) {
+            clickedCards.push(e.target.attributes.index.value);
+            setScore(score + 1);
+        } else {
+            console.log('you lose');
+        }
     }
 
     return (
@@ -22,7 +26,12 @@ function App() {
             <Header score={score} level={level} />
             <div className='gameboard'>
                 {images.map((image, index) => (
-                    <Card image={image} key={index} onClick={updateScore} />
+                    <Card
+                        image={image}
+                        key={index}
+                        index={index}
+                        onClick={updateScore}
+                    />
                 ))}
             </div>
         </div>
--- memory-card-app/src/components/Card.js
+++ memory-card-app/src/components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
     return (
         <div className='card' onClick={props.onClick}>
             <div className='card-body'>
-                <img src={props.image} alt='card'></img>
+                <img src={props.image} alt='card' index={props.index}></img>
             </div>
         </div>
     );
