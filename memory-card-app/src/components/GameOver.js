5ea67d5bdba4fd3792e7fbf8bdbe772606c53e8f
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-28 23:42:24 -0500

    Change colors for .game-over and .level-up

--- memory-card-app/src/components/GameOver.js
+++ memory-card-app/src/components/GameOver.js
@@ -2,7 +2,9 @@ const GameOver = (props) => {
     return (
         <div className='game-over'>
             <h1>Game Over</h1>
-            <p>Your score: {props.score}</p>
+            <p>
+                Your score: <span className='final-score'>{props.score}</span>
+            </p>
             <button onClick={props.onClick}>Play Again</button>
         </div>
     );
